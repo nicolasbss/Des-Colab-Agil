c876d9a7bae8bfdc7a36a8c413308683a07b99b6
Nicolas <nicolasbs@al.insper.edu.br>
2018-03-12 12:09:08 -0300

    Incremento 5

---
+++ NicolasStegmann/src/main/java/br/pro/hashi/ensino/desagil/tequilada/CpuPlayer.java
@@ -0,0 +1,13 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+
+public class CpuPlayer extends Player{
+	protected int row;
+	protected int col;
+
+	public CpuPlayer(int row, int col) {
+		super(row, col);
+	}
+	public void move() {
+		
+	}
+}
---
+++ NicolasStegmann/src/main/java/br/pro/hashi/ensino/desagil/tequilada/HumanPlayer.java
@@ -0,0 +1,10 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+
+public class HumanPlayer extends Player{
+	protected int row;
+	protected int col;
+
+	public HumanPlayer(int row, int col) {
+		super (row,col);
+	}
+}
